bb3f8c98cca507c8c7c0de09c96634548d73f60b
Adrik976 <asondarj@prismamedia.com>
2020-01-22 16:19:25 +0100

    fix backend function

--- backend.py
+++ backend.py
@@ -9,33 +9,40 @@ import boto3
 # see https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/GettingStarted.Python.html
 # to use DynamoDB local include endpoint_url, to use dynamoDB web service, remove endpoint_url
 #dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
-dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url='http://localhost:8000')
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url='http://localhost:8100', aws_access_key_id='DEFAULT_ACCESS_KEY', aws_secret_access_key='DEFAULT_SECRET' )
+#client = boto3.client('dynamodb', region_name='us-west-2', endpoint_url='http://localhost:8100')
 
-def create(event, context):
+def handler(event, context):
     data = json.loads(event['body'])
-    if len(data.review) == 0:
-        logging.error("review text is empty")
+    if len(data) != 2:
+        logging.error("Error on review")
         raise Exception("Couldn't insert prediction result to db.")
-
+    review = data["review"]
+    sentiment = data["sentiment"]
+    
     timestamp = str(datetime.utcnow().timestamp())
 
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
-
+    
     item = {
-        'classification': data.sentiment,
+        'classification': sentiment,
         'created_at': timestamp,
-        'review': data.review,
+        'review': review,
         'verified': True,
-        'id': hashlib.sha256(data.review.encode()),
+        'id': hashlib.md5(review.encode()).hexdigest(),
     }
 
-    # write the todo to the database
+    # # write the todo to the database
     table.put_item(Item=item)
 
-    # create a response
+    # # create a response
+    # response = {
+    #     "statusCode": 200,
+    #     "body": json.dumps(item)
+    # }
     response = {
         "statusCode": 200,
-        "body": json.dumps(item)
+        "body": json.dumps(data)
     }
 
     return response
